2b9a58aff8c368aa806fa4f9f55d6390de21f4a2
Илья Семёнов <71377999+Yny4ii@users.noreply.github.com>
2021-12-19 16:46:47 +0300

    Create insertScript.sql

---
+++ insertScript.sql
@@ -0,0 +1,12 @@
+insert into category (name)values ('health');
+insert into category (name)values ('gifts');
+insert into category (name)values ('groceries');
+insert into category (name)values ('education');
+
+insert into expense (amount, local_date, name, category_id)  values (22, '2021-12-14', 'flowers', 2);
+insert into expense (amount, local_date, name, category_id)  values (2, '2021-12-14', 'toy', 2);
+insert into expense (amount, local_date, name)  values (1000, '2021-11-10', 'phone');
+insert into expense (amount, local_date, name, category_id)  values (1, '2021-10-1', 'bread', 3);
+insert into expense (amount, local_date, name, category_id)  values (2, '2021-10-1', 'vegetables', 3);
+insert into expense (amount, local_date, name, category_id)  values (4, '2021-10-2', 'meet', 3);
+insert into expense (amount, local_date, name, category_id)  values (40, '2021-07-25', 'teeth', 1);
